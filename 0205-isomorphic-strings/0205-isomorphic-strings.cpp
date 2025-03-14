e1404e35aa1382f7bd988e1e13bc648d8bf982cf
Kunam Day Harika <155179625+DAYHARIKA@users.noreply.github.com>
2025-03-14 17:27:39 +0530

    Time: 0 ms (100%), Space: 9.4 MB (28.5%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        int arr_s[256]={0};
+        int arr_t[256]={0};
+        for(int i=0;i<s.length();i++){
+            if(arr_s[s[i]] == 0 && arr_t[t[i]] == 0){
+                arr_s[s[i]]=t[i];
+                arr_t[t[i]]=s[i];
+            }else if(arr_s[s[i]] != t[i] || arr_t[t[i]] != s[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+};

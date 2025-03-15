38fe59dbf6ab180a125cef7c7431d2d09d047a86
Kunam Day Harika <155179625+DAYHARIKA@users.noreply.github.com>
2025-03-15 13:27:03 +0530

    Time: 0 ms (100%), Space: 9.6 MB (74.14%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -3,14 +3,15 @@ public:
     bool isAnagram(string s, string t) {
 
          if(s.length() != t.length())return false;
-         int arr1[256]={0};
-         int arr2[256]={0};
+         int arr1[26]={0};
          for(int i=0;i<s.length();i++){
-            arr1[s[i]]++;
-            arr2[t[i]]++;
+            arr1[s[i]-'a']++;
+         }
+         for(int i=0;i<t.length();i++){
+            arr1[t[i]-'a']--;
          }
          for(int i=0;i<s.length();i++){
-            if(arr1[s[i]] != arr2[s[i]])return false;
+            if(arr1[s[i]-'a'] != 0)return false;
          }
          return true;
     }

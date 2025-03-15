98605c5a16ee78c1d6cef6483a3810e47fb7e8cb
Kunam Day Harika <155179625+DAYHARIKA@users.noreply.github.com>
2025-03-15 10:47:42 +0530

    Time: 15 ms (1.27%), Space: 8.2 MB (60.79%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.cpp
+++ 0812-rotate-string/0812-rotate-string.cpp
@@ -3,9 +3,23 @@ public:
     
     bool rotateString(string s, string goal) {
         
-        if(s.length() == 0)return true;
-        if(goal.length() != s.length())return false;
-        string news=s+s;
-        return news.find(goal)<news.length();
+        if(s.length()!=goal.length())
+        return false;
+
+        string temp=s+s;
+        int i=0;int j=0;
+        while(i<temp.length()){
+            cout<<temp[i]<<" "<<goal[j]<<endl;
+            if(temp[i]==goal[j]){
+                j++;
+            }
+            else{
+                i=i-j;
+                j=0;
+            }
+            if(j>=goal.length()) break;
+            i++;
+        }
+        return j==goal.length();
     }
 };

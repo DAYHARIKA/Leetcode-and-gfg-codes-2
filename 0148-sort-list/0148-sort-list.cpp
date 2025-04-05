23b5c4f835ab9c141eb72f4a6fe8b862cfe9a017
Kunam Day Harika <155179625+DAYHARIKA@users.noreply.github.com>
2025-04-05 11:34:58 +0530

    Time: 47 ms (40.94%), Space: 75.4 MB (48.02%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.cpp
@@ -0,0 +1,65 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        if(head == NULL || head->next == NULL){
+            return head;
+        }
+        ListNode* slow=head;
+        ListNode* fast=head->next;
+        while(fast != NULL && fast->next != NULL){
+             
+            slow=slow->next;
+            fast=fast->next->next;
+
+        }
+        return slow;
+    }
+    ListNode* mergeList(ListNode* left,ListNode* right){
+        ListNode* dummy=new ListNode(0);
+        ListNode* ptr=dummy;
+        while(left != NULL && right != NULL){
+            if(left->val <= right->val){
+                ptr->next=left;
+                ptr=left;
+                left=left->next;
+            }else{
+                ptr->next=right;
+                ptr=right;
+                right=right->next;
+            }
+        }
+        while(left != NULL){
+            ptr->next=left;
+            ptr=left;
+            left=left->next;
+        }
+        while(right != NULL){
+            ptr->next=right;
+            ptr=right;
+            right=right->next;
+        }
+        return dummy->next;
+    }
+    ListNode* sortList(ListNode* head) {
+        if(head == NULL || head->next == NULL){
+            return head;
+        }
+        ListNode* middle=middleNode(head);
+        ListNode* lefthead=head;
+        ListNode* righthead=middle->next;
+        middle->next=NULL;
+        lefthead=sortList(lefthead);
+        righthead=sortList(righthead);
+        return mergeList(lefthead,righthead);
+    }
+};

98ec124bc5d9b0336e61783887e51f8cb61a1a62
Kunam Day Harika <155179625+DAYHARIKA@users.noreply.github.com>
2025-06-05 12:52:28 +0530

    Time: 595 ms (5.08%), Space: 100.6 MB (5.33%) - LeetHub

--- 0282-expression-add-operators/0282-expression-add-operators.cpp
+++ 0282-expression-add-operators/0282-expression-add-operators.cpp
@@ -1,32 +1,41 @@
 class Solution {
 public:
-    void solve(string num,vector<string>& ans,string temp,int target,long sum,long prev,int ind){
-        if(ind == num.size()){
-            if(sum == target){
-                ans.push_back(temp);
+    vector<string> addOperators(string num, int target) {
+    vector<string> results;
+
+    // Internal recursive function to build expressions
+    function<void(int, long, long, string)> build = [&](int index, long eval, long last, string expr) {
+        if (index == num.size()) {
+            if (eval == target) {
+                results.push_back(expr);
             }
             return;
         }
-        for(int i=ind;i<num.length();i++){
-            if(i > ind && num[ind] == '0')break;
-            string substr=num.substr(ind,i-ind+1);
-            long sub=stol(substr);
-            if(ind==0){
-                solve(num,ans,temp+substr,target,sub,sub,i+1);
-            }else{
-                solve(num,ans,temp+'+'+substr,target,sub+sum,sub,i+1);
-                solve(num,ans,temp+'-'+substr,target,sum-sub,-sub,i+1);
-                solve(num,ans,temp+'*'+substr,target,sum-prev+(prev*sub),prev*sub,i+1);
-            }
 
+        for (int j = index; j < num.size(); ++j) {
+            // Prevent numbers like "05", "00" etc.
+            if (j > index && num[index] == '0') break;
+
+            string current = num.substr(index, j - index + 1);
+            long value = stol(current);
+
+            if (index == 0) {
+                // First number, just start the expression
+                build(j + 1, value, value, current);
+            } else {
+                // Addition
+                build(j + 1, eval + value, value, expr + '+' + current);
+                // Subtraction
+                build(j + 1, eval - value, -value, expr + '-' + current);
+                // Multiplication (handle precedence)
+                build(j + 1, eval - last + last * value, last * value, expr + '*' + current);
+            }
         }
-    }
-    vector<string> addOperators(string num, int target) {
-          
-        int n=num.length();
-        vector<string> ans;
-        string temp="";
-        solve(num,ans,temp,target,0,0,0);
-        return ans;
-    }
+    };
+
+    // Start recursion
+    build(0, 0, 0, "");
+    return results;
+}
+
 };

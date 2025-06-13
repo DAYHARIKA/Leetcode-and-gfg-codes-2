0a831b470d64a24bfcc3110753f22b198c2a19aa
Kunam Day Harika <155179625+DAYHARIKA@users.noreply.github.com>
2025-06-13 13:33:59 +0530

    Time: 0 ms (100%), Space: 8.8 MB (62.34%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        int n=s.length();
+        for(int i=0;i<n;i++){
+
+            if(st.empty() && (s[i] == ')' || s[i] == ']' || s[i] == '}')){
+                return false;
+            }else if(s[i] == ')' && st.top() == '('){
+                st.pop();
+            }else if(s[i] == ']' && st.top() == '['){
+                st.pop();
+            }else if(s[i] == '}' && st.top() == '{'){
+                st.pop();
+            }else{
+                st.push(s[i]);
+            }
+        }
+        return st.empty();
+    }
+};

76a63e4633a79d140e1098291d12bf1046f38a86
Kunam Day Harika <155179625+DAYHARIKA@users.noreply.github.com>
2025-06-30 13:43:45 +0530

    Time: 3 ms (72.5%), Space: 10.8 MB (70.41%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -12,14 +12,13 @@ public:
             
             hash[s[r]-'A']++;
             maxfreq=max(maxfreq,hash[s[r]-'A']);
-            while((r-l+1)-maxfreq > k){
+            if((r-l+1)-maxfreq > k){
                 hash[s[l]-'A']--;
-                maxfreq=0;
-                for(int i=0;i<26;i++)
-                   maxfreq=max(maxfreq,hash[i]);
                 l++;
             }
+            if((r-l+1)-maxfreq <= k)
             maxlen=max(maxlen,r-l+1);
+
             r++;
         }
         return maxlen;

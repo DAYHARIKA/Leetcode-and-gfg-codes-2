21ede3ee42849f88f42897fe1288757c56f6e13a
Kunam Day Harika <155179625+DAYHARIKA@users.noreply.github.com>
2025-07-20 12:12:57 +0530

    Time: 0 ms (100%), Space: 8.6 MB (50.87%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        
+        int l=0;
+        int h=x;
+
+        int ans=-1;
+
+        while(l <= h){
+            long mid=l+(h-l)/2;
+            if(mid*mid == x){
+               return mid;
+            }else if(mid*mid < x){
+                ans=mid;
+                l=mid+1;
+            }else{
+                h=mid-1;
+            }
+        }
+        return ans;
+    }
+};

6cf531ef9c3704c9a7bcf80e1585f4359a280736
Kunam Day Harika <155179625+DAYHARIKA@users.noreply.github.com>
2025-09-03 20:56:47 +0530

    Time: 4 ms (97.6%), Space: 34.8 MB (78.86%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -3,35 +3,32 @@ public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         int n=mat.size();
         int m=mat[0].size();
-        vector<vector<int>> visited(n,vector<int>(m,0));
-        vector<vector<int>> ans(n,vector<int>(m,0));
-        queue<pair<pair<int,int>,int>> q;
+       // vector<vector<int>> visited(n,vector<int>(m,0));
+        vector<vector<int>> ans(n,vector<int>(m,-1));
+        queue<pair<int,int>> q;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(mat[i][j] == 0){
-                    q.push({{i,j},0});
-                    visited[i][j]=1;
+                    q.push({i,j});
+                    ans[i][j]=0;
                 }
             }
         }
         vector<int> arr_row={-1,1,0,0};
         vector<int> arr_col={0,0,-1,1,};
         while(!q.empty()){
-            pair<pair<int,int>,int> temp=q.front();
+            pair<int,int> temp=q.front();
             q.pop();
-            int i=temp.first.first;
-            int j=temp.first.second;
-            int dist=temp.second;
-
-            ans[i][j]=dist;
+            int i=temp.first;
+            int j=temp.second;
 
             for(int k=0;k<4;k++){
                 int row=i+arr_row[k];
                 int col=j+arr_col[k];
 
-                if((row >= 0 && row < n) && (col >= 0 && col < m) && visited[row][col] != 1){
-                    q.push({{row,col},dist+1});
-                    visited[row][col]=1;
+                if((row >= 0 && row < n) && (col >= 0 && col < m) && ans[row][col] == -1){
+                    q.push({row,col});
+                    ans[row][col]=ans[i][j]+1;
                 }
             }
         }

ca6a6ef3a2cb0ce92dfb584c208bbdc0e5ec66ba
Kunam Day Harika <155179625+DAYHARIKA@users.noreply.github.com>
2025-09-05 13:12:29 +0530

    Time: 0 ms (100%), Space: 17.2 MB (97.99%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
@@ -5,13 +5,13 @@ private:
 
          for(int it:graph[node]){
              if(arr[it] == -1){
-                if(dfs(graph,arr,it,!color))return true;//not biaratite
+                if(!dfs(graph,arr,it,!color))return false;//not biaratite
              }else if(arr[it] == color){
-                return true;
+                return false;
              }
          }
 
-         return false;
+         return true;
     }
 public:
     bool isBipartite(vector<vector<int>>& graph) {
@@ -20,7 +20,7 @@ public:
         vector<int> arr(n,-1);
         for(int i=0;i<n;i++){
             if(arr[i] == -1){
-                if(dfs(graph,arr,i,0))return false;
+                if(!dfs(graph,arr,i,0))return false;
             }
         }
 

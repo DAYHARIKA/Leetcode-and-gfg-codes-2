9bc24c8c5e76591fc10f4d966638b31111277166
Kunam Day Harika <155179625+DAYHARIKA@users.noreply.github.com>
2025-09-18 21:24:09 +0530

    Time: 0 ms (100%), Space: 8.9 MB (36.13%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.cpp
+++ 0060-permutation-sequence/0060-permutation-sequence.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
     string getPermutation(int n, int k) {
+        vector<int> numbers;
+        for(int i=1;i <= n;i++){
+            numbers.push_back(i);
+        }
         k--;
         string result="";
-        vector<int> nums(n);
-        iota(nums.begin(),nums.end(),1);
-        vector<int> factorials(n);
-        factorials[0]=1;
+        int fact=1;
         for(int i=1;i<n;i++){
-            factorials[i]=factorials[i-1]*i;
+            fact *= i;
         }
-        for(int i=n-1;i>=0;i--){
-            int idx=k/factorials[i];
-            result += to_string(nums[idx]);
-            nums.erase(nums.begin()+idx);
-            k %= factorials[i];
+        for(int i=n;i>0;i--){
+            int index=k/fact;
+            result += to_string(numbers[index]);
+            numbers.erase(numbers.begin()+index);
+            if(i>1){
+                k %= fact;
+                fact /=(i-1);
+            }
         }
         return result;
     }

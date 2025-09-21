f145622fa2389bed84a4f6cdae8d963860f4e6cd
Kunam Day Harika <155179625+DAYHARIKA@users.noreply.github.com>
2025-09-21 12:21:14 +0530

    Time: 0 ms (100%), Space: 10.7 MB (22.51%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -13,7 +13,17 @@ public:
     int rob(vector<int>& nums) {
         int n=nums.size();
         vector<int> dp(n,-1);
-        int ans=solve(nums,dp,n,n-1);
-        return ans;
+        dp[0]=nums[0];
+        for(int i=1;i<n;i++){
+            int inclu=nums[i];
+            if(i-2 >= 0){
+                inclu += dp[i-2];
+            }
+            int exclu=dp[i-1];
+
+            dp[i]=max(inclu,exclu);
+        }
+        
+        return dp[n-1];
     }
 };

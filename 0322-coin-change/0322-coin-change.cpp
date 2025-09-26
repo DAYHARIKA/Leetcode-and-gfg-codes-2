6f43da20f76822753ddabf8ad9c0859838259146
Kunam Day Harika <155179625+DAYHARIKA@users.noreply.github.com>
2025-09-26 12:21:23 +0530

    Time: 23 ms (71.18%), Space: 17.9 MB (73.44%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -15,10 +15,21 @@ private:
     }
 public:
     int coinChange(vector<int>& coins, int amount) {
-        vector<int> dp(amount+1,-1);
-        int ans=solve(coins,amount,dp);
+        vector<int> dp(amount+1,INT_MAX);
+        dp[0]=0;
+
+        for(int i=1;i<=amount;i++){
+            int mini=INT_MAX;
+            for(int j=0;j<coins.size();j++){
+                if(i-coins[j] >= 0 && dp[i-coins[j]] != INT_MAX){
+                    int temp=dp[i-coins[j]];
+                    mini=min(mini,1+temp);
+                }   
+            }
+            dp[i]=mini;
+        }
         
-        if(ans == INT_MAX)return -1;
-        return ans;
+        if(dp[amount] == INT_MAX)return -1;
+        return dp[amount];
     }
 };

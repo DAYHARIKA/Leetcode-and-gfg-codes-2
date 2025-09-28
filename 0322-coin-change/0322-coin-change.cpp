5714edfd99614e51ef64b84216630922805e5473
Kunam Day Harika <155179625+DAYHARIKA@users.noreply.github.com>
2025-09-28 13:58:01 +0530

    Time: 24 ms (66.73%), Space: 17.9 MB (73.29%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,18 +1,5 @@
 class Solution {
-private:
-    int solve(vector<int>& coins, int amount, vector<int>& dp){
 
-        if(amount == 0)return 0;
-        if(amount < 0)return INT_MAX;
-        if(dp[amount] != -1)return dp[amount];
-        int mini=INT_MAX;
-        for(int i=0;i<coins.size();i++){
-            int temp=solve(coins,amount-coins[i],dp);
-            if(temp != INT_MAX)
-            mini=min(mini,1+temp);
-        }
-        return dp[amount]=mini;
-    }
 public:
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();

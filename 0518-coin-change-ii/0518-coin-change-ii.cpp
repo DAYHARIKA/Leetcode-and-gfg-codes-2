18b3b3ef52ec4786e2180c411c07a721fdaa6d0f
Kunam Day Harika <155179625+DAYHARIKA@users.noreply.github.com>
2025-09-28 13:07:31 +0530

    Time: 3 ms (94.72%), Space: 10.2 MB (88.51%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,25 +1,14 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
-    long long knapsack(int i, int a, vector<int>& coins) {
-        if (a == 0)
-            return 1;
-        if (i < 0)
-            return (a == 0) ? 1 : 0;
-        if (dp[i][a] != -1)
-            return dp[i][a];
-        else {
-            if (a >= coins[i]) {
-                int o1 = knapsack(i, a - coins[i], coins);
-                int o2 = knapsack(i - 1, a, coins);
-                return dp[i][a] = o1 + o2;
-            } else {
-                return dp[i][a] = knapsack(i - 1, a, coins);
+    int change(int value, vector<int>& d) {
+        vector<unsigned long long> prev(value + 1, 0);
+        prev[0] = 1;
+
+        for (int c:d) {
+            for (int j = c; j <= value; j++) {
+                prev[j]+=prev[j - c];
             }
         }
-    }
-    int change(int amount, vector<int>& coins) {
-        dp.assign(305,vector<int>(5005,-1));
-       return  knapsack(coins.size() - 1, amount, coins);
+        return prev[value];
     }
 };

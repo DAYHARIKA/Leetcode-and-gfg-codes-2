79af1f07d8009ffa462472affcb7ad05bf67cdb3
Kunam Day Harika <155179625+DAYHARIKA@users.noreply.github.com>
2025-11-06 13:04:21 +0530

    Time: 4 ms (38.15%), Space: 9.5 MB (90.99%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-
          if(s.length() != t.length())return false;
-         int arr1[256]={0};
+
+         int hashs[26]={0};
+         int hasht[26]={0};
+
          for(int i=0;i<s.length();i++){
-            arr1[s[i]]++;
+            hashs[s[i]-'a']++;
+            hasht[t[i]-'a']++;
          }
-         for(int i=0;i<t.length();i++){
-            arr1[t[i]]--;
-         }
+
          for(int i=0;i<s.length();i++){
-            if(arr1[s[i]] != 0)return false;
+            if(hashs[s[i]-'a'] != hasht[s[i]-'a'])return false;
          }
+
          return true;
     }
 };

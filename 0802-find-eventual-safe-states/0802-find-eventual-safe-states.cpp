c95d5c10330268bbb3ac0de9b6e9342076592453
Kunam Day Harika <155179625+DAYHARIKA@users.noreply.github.com>
2025-11-22 12:36:18 +0530

    Time: 10 ms (72.07%), Space: 51.8 MB (90.87%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -1,33 +1,31 @@
 class Solution {
 private:
-    bool dfs(vector<vector<int>>& graph,vector<int>& visited,vector<int>& pathvisited,int node){
+    bool dfs(vector<vector<int>>& graph,vector<int>& visited,int node){
          visited[node]=1;
-         pathvisited[node]=1;
 
          for(int it:graph[node]){
-            if(!visited[it]){
-                if(dfs(graph,visited,pathvisited,it) == false)return false;
-            }else if(pathvisited[it]){
+            if(visited[it] == 0){
+                if(dfs(graph,visited,it) == false)return false;
+            }else if(visited[it] == 1){
                 return false;
             }
          }
-         pathvisited[node]=0;
+         visited[node]=2;
          return true;
     }
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         int n=graph.size();
         vector<int> visited(n,0);
-        vector<int> pathvisited(n,0);
         vector<int> ans;
         for(int i=0;i<n;i++){
             if(!visited[i]){
-                dfs(graph,visited,pathvisited,i);
+                dfs(graph,visited,i);
             }
         }
 
         for(int i=0;i<n;i++){
-            if(pathvisited[i] == 0){
+            if(visited[i] == 2){
                ans.push_back(i);
             }
         }

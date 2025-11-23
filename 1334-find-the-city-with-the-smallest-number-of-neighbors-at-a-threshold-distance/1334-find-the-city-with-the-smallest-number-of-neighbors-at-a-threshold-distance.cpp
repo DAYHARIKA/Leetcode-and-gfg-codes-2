75c5b868598e8a71e19bdf67733df09d3674b0c4
Kunam Day Harika <155179625+DAYHARIKA@users.noreply.github.com>
2025-11-23 13:15:15 +0530

    Time: 38 ms (12.33%), Space: 20.5 MB (19.01%) - LeetHub

--- 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -3,30 +3,39 @@ public:
     int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
 
         vector<vector<int>> mat(n,vector<int>(n,1e9));
+        vector<vector<pair<int,int>>> adj(n);
 
         for(int i=0;i<edges.size();i++){
             int u=edges[i][0];
             int v=edges[i][1];
             int w=edges[i][2];
-
-            mat[u][v]=w;
-            mat[v][u]=w;
+            adj[u].push_back({v,w});
+            adj[v].push_back({u,w});
         }
 
+
+        //priority queue
         for(int i=0;i<n;i++){
+            priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+            pq.push({0,i});
             mat[i][i]=0;
-        }
+            while(!pq.empty()){
+                auto it=pq.top();
+                pq.pop();
 
-        for(int k=0;k<n;k++){
-            for(int i=0;i<n;i++){
-                for(int j=0;j<n;j++){
-                    if(mat[i][k] != 1e9 && mat[k][j] != 1e9){
-                        mat[i][j]=min(mat[i][j],mat[i][k]+mat[k][j]);
+                int dist=it.first;
+                int node=it.second;
+
+                for(auto it:adj[node]){
+                    int adj_node=it.first;
+                    int adj_dist=it.second;
+                    if(mat[i][adj_node] > dist+adj_dist){
+                        mat[i][adj_node]=dist+adj_dist;
+                        pq.push({mat[i][adj_node],adj_node});
                     }
                 }
             }
         }
-
         int cnt=n;
         int node=-1;
         

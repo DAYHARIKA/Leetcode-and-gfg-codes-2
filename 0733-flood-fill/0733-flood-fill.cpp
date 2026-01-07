edc7b521c43cc15efaf5cb0b1336636235dc8ee5
Kunam Day Harika <155179625+DAYHARIKA@users.noreply.github.com>
2026-01-07 12:42:13 +0530

    Time: 1 ms (20.17%), Space: 18.2 MB (55.15%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,41 +1,35 @@
 class Solution {
 public:
+    void dfs(vector<vector<int>>& image, int i, int j, int color, int req){
+         image[i][j]=color;
+         int n=image.size();
+         int m=image[0].size();
+         vector<int> drow={-1,1,0,0};
+         vector<int> dcol={0,0,-1,1};
+
+        for(int k=0;k<4;k++){
+            int row=i+drow[k];
+            int col=j+dcol[k];
+            if((row >= 0 && row < n) && (col >= 0 && col < m) && image[row][col] == req && image[row][col] != color){
+                image[row][col]=color;
+                dfs(image,row,col,color,req);
+            }
+        }
+         
+    }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         
         int n=image.size();
         int m=image[0].size();
-        vector<vector<int>> ans(n,vector<int>(m));
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                ans[i][j]=image[i][j];
-            }
-        }
+        //vector<vector<int>> ans(n,vector<int>(m));
+        
         int reqcolor=image[sr][sc];
 
-        if(color == reqcolor)return ans;
+        if(color == reqcolor)return image;
         
-        queue<pair<int,int>> q;
-        q.push({sr,sc});
-        ans[sr][sc]=color;
-        
-        vector<int> drow={-1,1,0,0};
-        vector<int> dcol={0,0,-1,1};
-        while(!q.empty()){
-            pair<int,int> node=q.front();
-            q.pop();
-            int i=node.first;
-            int j=node.second;
-            for(int k=0;k<4;k++){
-                int row=i+drow[k];
-                int col=j+dcol[k];
-                if((row >= 0 && row < n) && (col >= 0 && col < m) && image[row][col] == reqcolor && ans[row][col] != color){
-                    q.push({row,col});
-                    ans[row][col]=color;
-                }
-            }
-        }
+        dfs(image,sr,sc,color,reqcolor);
 
-        return ans;
+        return image;
 
     }
 };

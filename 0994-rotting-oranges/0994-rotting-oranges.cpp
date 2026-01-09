d592f1f36c1e6648c59df6805d7a8d9af880b112
Kunam Day Harika <155179625+DAYHARIKA@users.noreply.github.com>
2026-01-09 10:01:50 +0530

    Time: 0 ms (100%), Space: 17.4 MB (13.88%) - LeetHub

---
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        
+        int n=grid.size();
+        int m=grid[0].size(); 
+        queue<pair<pair<int,int>,int>> q;
+        vector<vector<int>> visit(n,vector<int>(m,0));
+        int cntfresh=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j] == 2){
+                    q.push({{i,j},0});
+                    visit[i][j]=1;
+                }else if(grid[i][j] == 1){
+                    cntfresh++;
+                }
+            }
+        }
+
+        int time=0;
+        int cnt=0;
+        vector<int> drow={-1,1,0,0};
+        vector<int> dcol={0,0,-1,1};
+        while(!q.empty()){
+            pair<pair<int,int>,int> node=q.front();
+            q.pop();
+            int i=node.first.first;
+            int j=node.first.second;
+            int t=node.second;
+            time=max(time,t);
+
+           for(int k=0;k<4;k++){
+              int row=i+drow[k];
+              int col=j+dcol[k];
+
+              if((row >= 0 && row < n) && (col>=0 && col < m) && visit[row][col] != 1 && grid[row][col] != 0){
+                 q.push({{row,col},t+1});
+                 visit[row][col]=1;
+                 cnt++;
+              }
+           }
+
+        }
+
+        if(cnt != cntfresh )return -1;
+
+        return time;
+        
+    }
+};

3a8864c4c78416943ff92a84573561455b56d863
Kunam Day Harika <155179625+DAYHARIKA@users.noreply.github.com>
2026-03-16 21:07:58 +0530

    Time: 0 ms (100%), Space: 8.2 MB (80.25%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,35 +1,25 @@
 class Solution {
-private:
-    int solve(int m, int n){
-        if(m == 0 && n == 0)return 1;
-
-        if(m < 0 || n < 0)return 0;
-
-        int up=solve(m-1,n);
-        int right=solve(m,n-1);
-
-        return up+right;
-    }
 public:
     int uniquePaths(int m, int n) {
+        
+        vector<int> curr(n,0);
         vector<int> prev(n,0);
 
         for(int i=0;i<m;i++){
-            vector<int> temp(n,0);
             for(int j=0;j<n;j++){
-                if(i == 0 && j == 0)temp[j]=1;
+                if(i== 0 && j==0)curr[0]=1;
                 else{
+                    int left=0;
+                    if(j-1 >= 0)left=curr[j-1];
                     int up=0;
-                    int right=0;
-                    if(i>0)up=prev[j];
-                    if(j>0)right=temp[j-1];
-                    temp[j]=up+right;
+                    if(i-1 >= 0)up=prev[j];
+
+                    curr[j]=left+up;
                 }
             }
-            prev=temp;
+            prev=curr;
         }
 
-        
         return prev[n-1];
     }
 };

bf92d032751a6aa31a66fb2797b5b75ede32ae7e
Kunam Day Harika <155179625+DAYHARIKA@users.noreply.github.com>
2026-03-17 10:32:00 +0530

    Time: 0 ms (100%), Space: 12.8 MB (69.44%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -16,23 +16,23 @@ public:
         
         int n=triangle.size();
   
-        vector<vector<int>> dp(n,vector<int>(n,0));
+        vector<int> curr(n,0);
 
         for(int i=0;i<n;i++){
-            dp[n-1][i]=triangle[n-1][i];
+            curr[i]=triangle[n-1][i];
         }
         
         for(int i=n-2;i>=0;i--){
             for(int j=0;j<=i;j++){
 
-                int s=triangle[i][j]+dp[i+1][j];
-                int d=triangle[i][j]+dp[i+1][j+1];
+                int s=triangle[i][j]+curr[j];
+                int d=triangle[i][j]+curr[j+1];
 
-                dp[i][j]=min(s,d);
+                curr[j]=min(s,d);
             }
         }
         
 
-        return dp[0][0];
+        return curr[0];
     }
 };

3231f8246c7b847f19883bd00f584e76ddf9cf2e
Kunam Day Harika <155179625+DAYHARIKA@users.noreply.github.com>
2026-03-23 22:26:32 +0530

    Time: 132 ms (37.24%), Space: 53.8 MB (12.43%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,52 +1,24 @@
 class Solution {
-private:
-    int solve(vector<int>& nums, int n, int target){
-        if(n == 0){
-            if(nums[0] == 0 && target == 0)return 2;
-            if(target == 0)return 1;
-            return nums[0]==target;
-        }
-
-        int nottake=solve(nums,n-1,target);
-        int take=0;
-        if(nums[n] <= target){
-           take = solve(nums,n-1,target-nums[n]);
-        }
-
-        return take+nottake;
-    }
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
-        int n=nums.size();
-        int ts=0;
-        for(int i=0;i<n;i++){
-            ts += nums[i];
-        }
+        
+        unordered_map<int,int> prev;
+        prev[0]=1;
 
-        if(ts+target < 0 || (ts+target)%2)return 0;
+        for(int i=1;i<=nums.size();i++){
+            unordered_map<int,int> curr;
+            int x=nums[i-1];
+            
+            for(auto &[sum,ways]:prev){
 
-        int tar=(ts+target)/2;
-        vector<int> dp(tar+1,0);
-        // vector<int> curr(tar+1,0);
-
-        if(nums[0] == 0)dp[0]=2;
-        else dp[0]=1;
-
-        if(nums[0] != 0 && nums[0] <= tar)dp[nums[0]]=1;
- 
-        for(int i=1;i<n;i++){
-            for(int j=tar;j>=0;j--){
-                int nottake=dp[j];
-                int take=0;
-                if(nums[i] <= j){
-                take = dp[j-nums[i]];
-                }
-
-                dp[j]=take+nottake;
+                curr[sum+x] += ways;
+                curr[sum-x] += ways;
             }
-            // dp=curr;
+
+            prev=curr;
+
         }
 
-        return dp[tar];
-    }
+        return prev[target];
+    } 
 };

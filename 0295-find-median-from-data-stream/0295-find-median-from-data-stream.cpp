a0850c9a11910971917fb03e9b7a4f36adeca031
Kunam Day Harika <155179625+DAYHARIKA@users.noreply.github.com>
2026-07-17 23:06:26 +0530

    Time: 59 ms (85.77%), Space: 148.7 MB (29.93%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
@@ -1,38 +1,41 @@
 class MedianFinder {
 public:
+
     priority_queue<int> pq1;
-	priority_queue<int,vector<int>,greater<int>> pq2;
+    priority_queue<int,vector<int>,greater<int>> pq2;
+
     MedianFinder() {
         
     }
     
     void addNum(int num) {
-        //always insert element to left side when both are empty
-        //should follow property pq1.top() <= pq2.top() to get correct median
-        if(pq1.size()==0 || num<pq1.top()){
-			 pq1.push(num);
-		 }else{
-			 pq2.push(num);
-		 }
-        //to get correct median,always the difference between size of heaps should be either 1 or 0.
-         if(pq1.size()>pq2.size()+1){
-			 pq2.push(pq1.top());
-			 pq1.pop();
-		 }
-		 if(pq2.size()>pq1.size()+1){
-			 pq1.push(pq2.top());
-			 pq2.pop();
-		 }
+
+        if(pq1.size() == 0 || num < pq1.top()){
+            pq1.push(num);
+        }else{
+            pq2.push(num);
+        }
+
+        if(pq1.size() > pq2.size()+1){
+            pq2.push(pq1.top());
+            pq1.pop();
+        }
+
+        if(pq2.size() > pq1.size()+1){
+            pq1.push(pq2.top());
+            pq2.pop();
+        }
     }
     
     double findMedian() {
-        if(pq1.size()== pq2.size()){
-			 return (pq1.top()+pq2.top())/2.0;
-		 }else if(pq1.size()>pq2.size()){
-			 return pq1.top();
-		 }else{
-			  return pq2.top();
-		 }
+        
+        if(pq1.size() == pq2.size()){
+            return (pq1.top()+pq2.top())/2.0;
+        }else if(pq1.size() > pq2.size()){
+            return pq1.top();
+        }else{
+            return pq2.top();
+        }
     }
 };
 
